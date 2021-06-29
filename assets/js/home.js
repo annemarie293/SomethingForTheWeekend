3ff613cf54f72dbc0bd01a264d3b3c899e66c31a
annemarie293 <annemarie293@gmail.com>
2021-06-29 21:28:19 +0000

    added javascript function to change text on hero image on click

---
+++ assets/js/home.js
@@ -0,0 +1,6 @@
+
+
+$("#image-button").click(function(){
+  $(".hero-image-text h2").text("You've come to the right place. Click below and let us show you some of our favourite hidden gems to inspire your travels");
+  $(this).html('<button class="hero-image-button-two btn btn-lg"><a href="destinations.html">Find my dream holiday!</a></button>');
+});
