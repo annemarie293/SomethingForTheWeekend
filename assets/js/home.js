2d94630b9ecbe2fa02e2989b5126b0105fa4bc98
annemarie293 <annemarie293@gmail.com>
2021-06-29 21:54:40 +0000

    fixed button html and classes in home.js script, fixed links in navbar for all pages

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,5 +2,5 @@
 
 $("#image-button").click(function(){
   $(".hero-image-text h2").text("You've come to the right place. Click below and let us show you some of our favourite hidden gems to inspire your travels");
-  $(this).html('<button class="hero-image-button-two btn btn-lg"><a href="destinations.html">Find my dream holiday!</a></button>');
+  $(this).html('<a href="destinations.html">Find my dream holiday!</a>').removeClass("hero-image-button-one").addClass("hero-image-button-two");
 });
