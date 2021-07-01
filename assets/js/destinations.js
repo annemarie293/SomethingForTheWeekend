2510f3ed209a1237692854e57d6bb0bf7ddb4870
annemarie293 <annemarie293@gmail.com>
2021-07-01 22:43:14 +0000

    added google maps API to display Europe map on destinations page

---
+++ assets/js/destinations.js
@@ -0,0 +1,9 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("google-map"), {
+      zoom: 3,
+      center: { lat: 48.91473349834997  , lng: 14.654953491288719 },
+      
+    });
+    
+  }
+ 
