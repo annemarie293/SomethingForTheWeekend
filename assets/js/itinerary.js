60d5600b6e07b758a36f2d6b83c9667ac51ab5ee
annemarie293 <annemarie293@gmail.com>
2021-07-08 01:20:50 +0000

    tidying up

--- assets/js/itinerary.js
+++ assets/js/itinerary.js
@@ -6,8 +6,10 @@ window.onload = function() {
         emailjs.sendForm("service_r5f1ryo", "sftw", "#itinerary-form" )
             .then(function() {
                 console.log('SUCCESS!');
+                alert("Thanks for your request submission!");
             }, function(error) {
                 console.log('FAILED...', error);
+                alert("TWhoops! something went wrong, please try again");
             });
     });
 }
